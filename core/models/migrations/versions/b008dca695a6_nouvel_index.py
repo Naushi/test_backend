294aea05fb7b0f52ed58b94797515711436c395b
Naushi <juliette.vachin@epitech.eu>
2019-06-13 23:03:11 +0200

    add index via alembic

---
+++ core/models/migrations/versions/b008dca695a6_nouvel_index.py
@@ -0,0 +1,24 @@
+"""nouvel index
+
+Revision ID: b008dca695a6
+Revises: d171c1f135e8
+Create Date: 2019-06-12 14:05:05.211927
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b008dca695a6'
+down_revision = 'd171c1f135e8'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_index('index_user', 'transaction', ['id', 'user_id'])
+
+
+def downgrade():
+    op.drop_index('index_user')
